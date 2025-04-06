ae6cf796f8c97b9515244a4a582a91da36e4e87f
matraass11 <fedor.kurmanov@gmail.com>
2025-04-06 22:44:24 +0200

    parent pointerss are now const

--- grad.cpp
+++ grad.cpp
@@ -5,7 +5,7 @@ class Product;
 class Value {
 public:
     double data;
-    Value *parent1, *parent2;
+    Value * const parent1, * const parent2;
     double grad = 1;
 
     Value(double data, Value* parent1=nullptr, Value* parent2=nullptr){
@@ -17,8 +17,8 @@ public:
         // if self is result of addition and dself/dchild = 2, then we increment parents' grads by 2*1
         std::cout << "backward undefined" << std::endl;
     }
-    Sum operator+(Value other);
-    Product operator*(Value other);
+    Sum operator+(const Value& other) const;
+    Product operator*(const Value& other) const;
 }; 
 
 class Sum : public Value {
@@ -32,7 +32,7 @@ public:
     }
 };
 
-Sum Value::operator+(Value other){
+Sum Value::operator+(const Value& other) const{
     double sum = data+other.data;
     Sum child(sum, this, &other);
     return child;
@@ -48,20 +48,22 @@ public:
     }
 };
 
-Product Value::operator*(Value other){
+Product Value::operator*(const Value& other) const{
     double product = data * other.data;
     Product child(product, this, &other);
     return child;
 }
 
 int main(){
-    Value r1c1(3), r1c2(3), r2c1(4), r2c2(6), n(-1);
-    Product pr = r1c1*r1c2;
+    Value r1c1(3), r1c2(5), r2c1(4), r2c2(6), n(-1);
+    Product pr1 = r1c1*r1c2;
+    Product pr2(15.0, &r1c1, &r1c2);
+    Product *ptr1 = &pr1, *ptr2 = &pr2;
 
-    // std::cout << pr.parent1 << std::endl;
+    // std::cout << pr1.data << r1c1.data << std::endl;
 
     Sum det = (r1c1 * r2c2) + (n * r2c1 * r1c2);
-    det.propagateBack();
+    // det.propagateBack();
     // std::cout << det.parent1 << "buhaha" << std::endl;
     
 }
