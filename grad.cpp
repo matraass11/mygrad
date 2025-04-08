523ad38db612422d160f2a43643615dabb835da9
matraass11 <fedor.kurmanov@gmail.com>
2025-04-08 14:44:26 +0200

    added backward(), propagateBack() -> virtual propagateFurther(), parent pointers not const. the operators only accept lvalues as the argument (the argument is on the right of an operator)

--- grad.cpp
+++ grad.cpp
@@ -5,34 +5,36 @@ class Product;
 class Value {
 public:
     double data;
-    Value * const parent1, * const parent2;
-    double grad = 1;
+    Value * parent1, *parent2;
+    double grad = 0;
 
-    Value(double data, Value* parent1=nullptr, Value* parent2=nullptr){
-        this->data = data, this->parent1 = parent1, this->parent2 = parent2;
+    Value(double data, Value* parent1=nullptr, Value* parent2=nullptr) : 
+        data(data), parent1(parent1), parent2(parent2) {}
+
+    virtual void propagateFurther(){
+        std::cout << this->data <<" backward undefined" << std::endl;
     }
 
-    void propagateBack(){
-        // increment parents' grads knowing the grad of the self and how self came to be
-        // if self is result of addition and dself/dchild = 2, then we increment parents' grads by 2*1
-        std::cout << "backward undefined" << std::endl;
+    void backward(){
+        grad = 1;
+        propagateFurther();
     }
-    Sum operator+(const Value& other) const;
-    Product operator*(const Value& other) const;
+
+    Sum operator+(Value& other);
+    Product operator*(Value& other);
 }; 
 
 class Sum : public Value {
 using Value::Value;
 public:
-    void propagateBack(){
-        parent1->grad += grad*1;
-        parent2->grad += grad*1;
-        parent1->propagateBack(), parent2->propagateBack();
-        std::cout << "parent1: " << parent1->data << std::endl;
+    void propagateFurther() override {
+        parent1->grad += grad*1, parent2->grad += grad*1;
+        std::cout << this->data << " propagated successfully" << std::endl;
+        parent1->propagateFurther(), parent2->propagateFurther();
     }
 };
 
-Sum Value::operator+(const Value& other) const{
+Sum Value::operator+(Value& other){
     double sum = data+other.data;
     Sum child(sum, this, &other);
     return child;
@@ -41,29 +43,32 @@ Sum Value::operator+(const Value& other) const{
 class Product : public Value {
 using Value::Value;
 public:
-    void propagateBack(){
-        parent1->grad += parent2->data, parent2->grad += parent1->data;
-        parent1->propagateBack(), parent2->propagateBack();
-        // std::cout << "parent1: " << parent1->data << std::endl;
+    void propagateFurther() override {
+        parent1->grad += grad * parent2->data, parent2->grad += grad * parent1->data;
+        std::cout << this->data << " propagated successfully" << std::endl;
+        parent1->propagateFurther(), parent2->propagateFurther();
     }
 };
 
-Product Value::operator*(const Value& other) const{
+Product Value::operator*(Value& other) {
     double product = data * other.data;
+    // std::cout << &other << std::endl;
     Product child(product, this, &other);
     return child;
 }
 
 int main(){
     Value r1c1(3), r1c2(5), r2c1(4), r2c2(6), n(-1);
-    Product pr1 = r1c1*r1c2;
-    Product pr2(15.0, &r1c1, &r1c2);
-    Product *ptr1 = &pr1, *ptr2 = &pr2;
+    Product pr1 = r1c1*r2c2, pr2 = r2c1 * r1c2 * n;
+    Product mul = (r1c1 + r2c2) * r1c2 * r2c1; 
+    //reason why this works and mul2 doesn't is 
+    // that the operator only expects the second arg to be an lvalue (i think)
+    Sum mul2 = n + r1c1 * r2c2 * r1c2 * r2c1;
+    // Sum mul = n + pr1;
+    mul.backward();
 
-    // std::cout << pr1.data << r1c1.data << std::endl;
-
-    Sum det = (r1c1 * r2c2) + (n * r2c1 * r1c2);
-    // det.propagateBack();
+    std::cout << r1c1.grad << std::endl;
+    // det.propagateFurther();
     // std::cout << det.parent1 << "buhaha" << std::endl;
     
 }
