ec0978a791936a830d4dfe76047ea051687c4eb4
matraass11 <fedor.kurmanov@gmail.com>
2025-04-20 20:09:34 +0200

    tensor class can now be initialized! yay!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "tensor.h"
 
+#include <array>
+
 int main(){
-    Tensor weights = Tensor(3, 4);
+    Tensor weights = Tensor(std::vector<int> {3, 4});
+
 }
--- tensor.h
+++ tensor.h
@@ -1,38 +1,51 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <random>
 #include "grad.h"
-class Value;
+// class Value;
 
 class Tensor {
 public:
-    int rowsN, columnsN;
-    // std::array<Value, rows> weights; 
+    std::vector<int> dimensions;
+    Value* values_array;
+    int length = 1;
 
-    Value*** rows;
+    explicit Tensor(std::vector<int> dimensions)
+    : dimensions(dimensions) {
+        for (auto i: dimensions){
+            length *= dimensions[i];
+        }
+        values_array = new Value[length]; //delete[] ? 
+                                         //is this a proper way to initialize my object?
+    }
 
-    explicit Tensor(int rowsN, int columnsN):
-    rowsN(rowsN), columnsN(columnsN)
+    explicit Tensor(Tensor& IndexedInto, int index) // constructor specifically for indexing
+    : length(IndexedInto.length / IndexedInto.dimensions[0])
     {
-        rows = new Value**[rowsN];
-        for (int i=0; i<rowsN; i++){
-            rows[i] = new Value*[columnsN];
+        for (int i=1; i<IndexedInto.length; i++){
+            dimensions[i-1] = IndexedInto.dimensions[i];
+        }
+        values_array = IndexedInto.values_array + index * length * sizeof(Value);
+    }
+    
+    
+    Tensor operator[](int index){
+        if (this->dimensions.size() > 1){ 
+            Tensor subTensor = Tensor(*this, index);
+            return subTensor;
         }
     }
-
-    // class Row {
-    // public:
-    //     int columns;
-    //     Row(c)
-    // }
     
-    Value** operator[](int row){
-        return values[columns*row + column];
-    }
-
-    void gaussianInit(){
-        ;
-    }
+    // void gaussianInit(){
+    //     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    //     std::default_random_engine generator(seed);
+    //     std::normal_distribution<double> d(0, 1);
+    //     for (int)
+    //         d(generator)
+    // }
 
     // void initAllTo(double value){
     //     for (int i; i < rows*columns)
