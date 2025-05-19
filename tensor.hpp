dfd69211200639680156ef7eb8985af316e9696b
matraass11 <fedor.kurmanov@gmail.com>
2025-05-19 23:07:52 +0200

    doesn't worl yet, just want to keep the matmul function in here

--- tensor.hpp
+++ tensor.hpp
@@ -2,19 +2,27 @@
 
 #include <iostream>
 
+class TensorBaseUntemplated {};
+
+template<size_t length, uint dimensionality>
+class TensorMatrixProduct;
 
 template <size_t length, uint dimensionality>
-class TensorBase {    
-private:
+class TensorBase : public TensorBaseUntemplated {    
+protected:
     std::array<double, length> dataArray;
     std::array<double, length> gradArray;
+    
+public:
     const std::array<uint, dimensionality> dimensions; 
     const std::array<uint, dimensionality> strides; 
+    
+protected:
     const TensorBase *parent1, *parent2;
 
-
 public:
-    TensorBase(std::array<double, length> dataArray, std::array<uint, dimensionality> dimensions) 
+
+    TensorBase(const std::array<double, length>& dataArray, const std::array<uint, dimensionality>& dimensions) 
     : dataArray(dataArray), gradArray({ }), dimensions(dimensions), strides(stridesFromDimensions(dimensions)) {
         uint productOfDimensions = 1;
         for (auto dim : dimensions){
@@ -24,16 +32,54 @@ public:
             std::cerr << "error: dimensions don't correspond to the length, exiting" << std::endl; 
             exit(1);
         }
+        // all this checking every time we construct a tensor?? maybe make a fast constructor
     }
-
+    
     void print() const {
         printRecursively(0, 0);
         std::cout << "\n";
     }
 
-private:
+    
+    double at(const std::array<uint, dimensionality>& indices){
+        return dataArray[indicesToLocationIn1dArray(indices)];
+    }
 
-    const std::array<uint, dimensionality> stridesFromDimensions(const std::array<uint, dimensionality>& dimensions){
+TensorBaseUntemplated matMul2d(const TensorBaseUntemplated& other) const {
+    // if (!(dimensionality == 2 && other.dimensionality == 2)) {
+    //     std::cerr << "error: matMul2d only for 2d, exiting\n";
+    //     exit(1);
+    // }
+    if (dimensions[1] != other.dimensions[0]) {
+        std::cerr << "error: invalid shapes, exiting" << std::endl; 
+        exit(1);
+    }
+
+    std::array<uint, 2> productDimensions = {dimensions[0], other.dimensions[1]};
+    const size_t productLength = dimensions[0] * other.dimensions[1];
+    std::array<double, productLength> productDataArray{ };
+
+    for (int row=0; row < productDimensions[0]; row++) {
+        for (int column=0; column < productDimensions[1]; column++) {
+            uint locationOfElementInProductDataArray = row * productDimensions[0] + column;
+            for (int dotProductIterator=0; dotProductIterator < dimensions[1]; dotProductIterator++) {
+                productDataArray[locationOfElementInProductDataArray] += at({row, dotProductIterator}) * other.at({dotProductIterator, column});
+            }
+        }
+    }
+
+    TensorMatrixProduct<productLength, 2> product(productDataArray, dimensions, this, &other);
+    return product;
+}
+
+
+protected:
+
+    TensorBase(const std::array<double, length>& dataArray, const std::array<uint, dimensionality>& dimensions, 
+               TensorBase* parent1, TensorBase* parent2) : TensorBase(dataArray, dimensions), 
+               parent1(parent1), parent2(parent2) {}
+    
+    const std::array<uint, dimensionality> stridesFromDimensions(const std::array<uint, dimensionality>& dimensions) const {
         std::array<uint, dimensionality> strides;
         strides[0] = length/dimensions[0];
         for (int i=1; i < dimensionality; i++){
@@ -41,7 +87,7 @@ private:
         }
         return strides;
     }
-
+    
     
     void printRecursively(uint start, uint dimension) const {
         uint increment = strides[dimension];
@@ -61,21 +107,20 @@ private:
         std::cout << "]";
     }
     
-    // TensorBase matMul2d(const TensorBase& other){
-    //     if (!(dimensionality == 2 && other.dimensionality == 2)){
-    //         std::cerr << "error: matMul2d only for 2d, exiting\n";
-    //         exit(1);
-    //     }
-    //     if (dimensions[1] != other.dimensions[0]){
-    //         std::cerr << "error: invalid shapes, exiting" << std::endl; 
-    //         exit(1);
-    //     }
+    uint indicesToLocationIn1dArray(const std::array<uint, dimensionality>& indices){
+        uint locationOfElementInDataArray = 0;
+        for (int i=0; i < dimensionality; i++){
+            locationOfElementInDataArray += indices[i] * strides[i];
+        }
+        if (locationOfElementInDataArray > length){
+            std::cerr << "error: invalid indices, exiting";
+            exit(1);
+        }
+        return locationOfElementInDataArray;
+    }
 
-    //     std::array<uint, 2> productDimensions = {dimensions[0], other.dimensions[1]};
-    //     size_t productLength = dimensions[0] * other.dimensions[1];
-    //     TensorMatrixProduct<productLength, 2> product(resultArray, dimensions)
-    // }
-    // virtual void propagateFurther();
+
+    // void propagateFurther();
 };
 
 
@@ -83,8 +128,9 @@ private:
 template <size_t length, uint dimensionality>
 class TensorMatrixProduct : public TensorBase <length, dimensionality> {
 public:
-    TensorMatrixProduct(std::array<double, length> dataArray) 
-    : TensorBase<length, dimensionality>(dataArray) {}
+    TensorMatrixProduct(const std::array<double, length>& dataArray, const std::array<uint, dimensionality>& dimensions, 
+                        const TensorBaseUntemplated* parent1, const TensorBaseUntemplated* parent2) 
+    : TensorBase<length, dimensionality>(dataArray, dimensions, parent1, parent2) {}
     
     // using TensorBase<length>::print;
     // void propagateFurther() override {
@@ -93,33 +139,3 @@ public:
         
         
 };
-
-
-
-
-
-
-
-    //     if (dimensionIndex > dimensionality - 1) {
-    //         std::cerr << "error: invalid dimension, exiting" << std::endl;
-    //         exit(1);
-    //     }
-    
-    //     uint stride = strides[dimensionIndex+1];
-    //     uint lastElement = indexOfFirstElement+lengthOfDimension;
-    //     std::cout << "[";
-    
-    //     for (int i=indexOfFirstElement; i < lastElement; i=i+stride){
-    //         if (dimensionIndex == dimensionality-2){
-    //             std::cout << dataArray[i];
-    //         }
-    //         else{
-    //             printOneDimension(dimensionIndex+1, i, lengthOfDimension/dimensions[dimensionIndex]);
-    //         }
-    //         if (i != lastElement-1){
-    //         std::cout << ", ";
-    //     }    
-    //     std::cout << "]";
-    // }
-    
-    // }
