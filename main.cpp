2fcb12659e1bb23b06f1a486ad19a54bdb1b29aa
matraass11 <fedor.kurmanov@gmail.com>
2025-06-04 20:12:29 +0200

    matMulProduct tensor struct

--- main.cpp
+++ main.cpp
@@ -4,22 +4,20 @@
 #include "tensorStruct.hpp"
 
 int main(){
-    tensorStruct<4> input({0.2, 2.1, 0.4, 1}, {1, 4});
-    tensorStruct<8> w1({1, 2, 3, 4, 5, 6, 7, 8}, {4, 2});
-    tensorStruct<2> w2({5, 7}, {2, 1});
+    tensor_s<4> input({0.2, 2.1, 0.4, 1}, {1, 4});
+    tensor_s<8> w1({1, 2, 3, 4, 5, 6, 7, 8}, {4, 2});
+    tensor_s<2> w2({5, 7}, {2, 1});
 
-    double p1Data[2], p1Grad[2] {};
-    TensorMatMulProduct p1(p1Data, p1Grad, input.tensor, w1.tensor);
-    double p2Data[1], p2Grad[1] {};
-    TensorMatMulProduct p2(p2Data, p2Grad, p1, w2.tensor);
+    tensor_matmul_product_s<2> p1(input.tensor, w1.tensor);
+    tensor_matmul_product_s<1> p2(p1.tensor, w2.tensor);
 
-    p1.matMul2dIntoSelf(input.tensor, w1.tensor);
-    p2.matMul2dIntoSelf(p1, w2.tensor);
-    p2.print();
+    p1.tensor.matMul2dIntoSelf(input.tensor, w1.tensor);
+    p2.tensor.matMul2dIntoSelf(p1.tensor, w2.tensor);
+    p2.tensor.print();
 
-    p2.setAllGradsTo(1);
-    p2.backwardFurther();
-    p1.backwardFurther(); 
+    p2.tensor.setAllGradsTo(1);
+    p2.tensor.backwardFurther();
+    p1.tensor.backwardFurther(); 
     w1.tensor.printGrad(); 
 
 } 
--- tensorStruct.hpp
+++ tensorStruct.hpp
@@ -1,16 +1,32 @@
 #pragma once
 
-class Tensor;
+// _s --> struct
 
-template<int length> // change to size_t
-struct tensorStruct {
+class Tensor;
+class TensorMatMulProduct;
+
+template<size_t length>
+struct tensor_s_base {
     double dataArray[length];
     double gradArray[length] = {};
+};
+
+template<size_t length> // change to size_t
+struct tensor_s : tensor_s_base<length> {
+
     Tensor tensor;
 
-    tensorStruct(const std::array<double, length>& dataArray, const std::vector<int>& dimensionsVector) :  
+    tensor_s(const std::array<double, length>& dataArray, const std::vector<int>& dimensionsVector) :  
                 tensor(Tensor(this->dataArray, this->gradArray, dimensionsVector)) {
                     for (int i=0; i<length; i++)
                         this->dataArray[i] = dataArray[i];
                 }
+};
+
+template <size_t length>
+struct tensor_matmul_product_s : tensor_s_base<length> {
+
+    TensorMatMulProduct tensor;
+    tensor_matmul_product_s(Tensor& leftParent, Tensor& rightParent) :
+        tensor(this->dataArray, this->gradArray, leftParent, rightParent) {}
 };
