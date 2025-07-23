bc8c3e1566cdab093004d949c9472f1f4e92586a
matraass11 <fedor.kurmanov@gmail.com>
2025-07-23 10:31:02 +0300

    filepaths with <filesystem>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
 #include <fstream>
+#include <filesystem>
 
 #include "src/tensor.hpp"
 #include "src/model.hpp"
@@ -12,10 +13,12 @@
 
 int main() {
 
-    Tensor images = load_mnist_images("/Users/fedorkurmanov/Desktop/prog/neuralNetCpp/datasets/mnist/train-images-ubyte");
-    Tensor imglabels = load_mnist_labels("/Users/fedorkurmanov/Desktop/prog/neuralNetCpp/datasets/mnist/train-labels-ubyte");
+    std::filesystem::path path = std::filesystem::current_path();
+
+    Tensor images = loadMnistImages(path /"../datasets/mnist/train-images-ubyte");
+    Tensor labels = loadMnistLabels(path /"../datasets/mnist/train-labels-ubyte");
     Tensor standartizedImages = standartize(images);
-    visualize_image_ascii(images, imglabels, 10);
+    visualizeImage(images, labels, 10);
 
     const size_t batchSize = 32;
 
@@ -26,12 +29,12 @@ int main() {
     int n_steps = 2;
 
     for (int step = 0; step < n_steps; step++) {
-        std::vector<size_t> indices = shuffledIndices(imglabels.length);
+        std::vector<size_t> indices = shuffledIndices(labels.length);
         dtype avgLoss = 0;
-        for (int batch = 0; batch < imglabels.length / batchSize; batch++) {
+        for (int batch = 0; batch < labels.length / batchSize; batch++) {
             std::vector<size_t> batchIndices = slicedIndices(indices, batch*32, 32);
             Tensor batchInputs = retrieveBatchFromData(standartizedImages, batchIndices);
-            Tensor batchLabels = retrieveBatchFromLabels(imglabels, batchIndices);
+            Tensor batchLabels = retrieveBatchFromLabels(labels, batchIndices);
 
             batchInputs.reshape({batchIndices.size(), 784});
 
@@ -48,19 +51,17 @@ int main() {
             model.zeroGrad();
 
         }
-        avgLoss /= (imglabels.length / batchSize);
+        avgLoss /= (labels.length / batchSize);
         std::cout << "average loss at " << step << ": " << avgLoss << "\n";
     }
 
 
-    // for (int i = 0; i < 1001; i++) {
-    //     std::memset(logits.grads.get(), 0, logits.length * sizeof(dtype));
-    //     dtype l = loss.forward(logits, labels);
-    //     loss.backward();
-    //     if (i % 100 == 0) {
-    //         std::cout << "loss at " << i << ": " << l << "\n";
-    //         logits.print();
-    //     }
-    //     optim.step();
-    // }
+
+    // Tensor testImages = loadMnistImages(path / "../datasets/mnist/test-images-ubyte");
+    // Tensor testLabels = loadMnistLabels(path / "../datasets/mnist/test-labels-ubyte");
+    // Tensor stdTestImages = standartize(images);
+    // visualizeImage(testImages, 88);
+
+    // const size_t testBatchSize = 32;
+    // for ()
 } 
