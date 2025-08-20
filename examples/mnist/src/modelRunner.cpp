350abc283258f91a68f11d4490fe8893faf9e5f7
matraass11 <fedor.kurmanov@gmail.com>
2025-08-20 22:34:05 +0200

    removed the break after 25 batches in training

--- examples/mnist/src/modelRunner.cpp
+++ examples/mnist/src/modelRunner.cpp
@@ -84,10 +84,10 @@ static dtype train(Model& model, Tensor& data, Tensor& labels, CrossEntropyLoss&
         model.zeroGrad();
 
 
-        // FOR SPEEDUP TESTING 
-        if (batch == 25) {
-            break;
-        }
+        // // FOR SPEEDUP TESTING 
+        // if (batch == 25) {
+        //     break;
+        // }
 
     }
     avgLoss /= (labels.length / batchSize);
