8019573e6425d9b70335260a3dce31e7b3584e37
matraass11 <fedor.kurmanov@gmail.com>
2025-08-31 14:05:36 +0200

    cli for cats, formatted printing of loss during training, commented out generateImages()

--- examples/cats/src/modelRunner.cpp
+++ examples/cats/src/modelRunner.cpp
@@ -7,44 +7,17 @@
 
 using namespace mygrad;
 
-static const size_t pixelsInImage = 64 * 64 * 3;
-static const size_t neurons = 1024;
-static const size_t latent = 128;
 
-
-void trainModel( Tensor& trainingImages ) {
+void trainModel( Model& encoder, Model& decoder, Tensor& trainingImages ) {
 
     for (size_t i = 0; i < trainingImages.length; i++) {
         trainingImages.data[i] /= 255.0; 
     }
 
 
-
-    Model encoder {
-        Reshape({1, pixelsInImage}, 0),
-        LinearLayer(pixelsInImage, neurons),
-        ReLU(),
-        LinearLayer(neurons, latent), // first 20 columns of output are the means, second 20 columns are the logvariances. 
-        // reparameterize simply interprets it that way 
-        // and outputs a tensor with values sampled from the distributions of shape [batchSize x latent]
-
-        ReLU(),
-    };
-
-    Reparameterize reparam;
-
-
-    Model decoder {
-        LinearLayer(latent, neurons),
-        ReLU(),
-        LinearLayer(neurons, pixelsInImage),
-        Sigmoid()
-    };
-
-
     std::vector<Tensor*> parameters = encoder.parameters;
-
     parameters.insert( parameters.end(), decoder.parameters.begin(), decoder.parameters.end() );
+
     Adam optim(parameters, 0.0001);
 
     KLdivWithStandardNormal kldiv;
@@ -58,32 +31,16 @@ void trainModel( Tensor& trainingImages ) {
         const size_t trainSize = data.dimensions[0];
 
         const std::vector<size_t> indices = shuffledIndices(trainSize);
-        dtype avgLoss = 0;
         for (int batch = 0; batch < trainSize / batchSize; batch++) {
-            std::vector<size_t> batchIndices = slicedIndices(indices, batch*batchSize, batchSize);
+            const std::vector<size_t> batchIndices = slicedIndices(indices, batch*batchSize, batchSize);
             Tensor batchInputs = retrieveBatchFromData(data, batchIndices);
-
-            
-            // Tensor& latentDistribution = encoder(batchInputs); 
-            // reparam.forward(latentDistribution);
-            // Tensor& outputs = decoder(reparam.outputTensor);
-            // dtype loss = kldiv(latentDistribution) + mse(outputs, batchInputs); // order is important
-
 
             Tensor& outputs = decoder(encoder(batchInputs));
             dtype loss = mse(outputs, batchInputs);
 
             if (batch % 5 == 0) {
-                std::cout << "batch - " << batch << ", loss - " << loss << "\n";
+                printf("%s%u%s%.4f\n", "batch - ", batch, ", loss - ", loss);
             }
-            // if (batch == 80) break;
-
-            avgLoss += loss;
-
-
-            // encoder.zeroGrad(), reparam.outputTensor.zeroGrad(), decoder.zeroGrad();
-            // mse.backward(), kldiv.backward();
-            // decoder.backward(), reparam.backward(), encoder.backward();
 
             encoder.zeroGrad(), decoder.zeroGrad();
             mse.backward(), decoder.backward(), encoder.backward();
@@ -91,35 +48,12 @@ void trainModel( Tensor& trainingImages ) {
             optim.step();
 
         }
-        avgLoss /= (trainSize / batchSize);
     }
-
-    encoder.save("../encoder.model");
-    decoder.save("../decoder.model");
 
 
 }
 
-void testModel( Tensor& testImages ) {
-
-
-    Model encoder {
-        Reshape({1, pixelsInImage}, 0),
-        LinearLayer(pixelsInImage, neurons),
-        ReLU(),
-        LinearLayer(neurons, latent), 
-        ReLU()
-    };
-
-    Model decoder {
-        LinearLayer(latent, neurons),
-        ReLU(),
-        LinearLayer(neurons, pixelsInImage),
-        Sigmoid()
-    };
-
-    encoder.load("../encoder.model");
-    decoder.load("../decoder.model");
+void testModel( Model& encoder, Model& decoder, Tensor& testImages, const std::string& dirForImages) {
 
     const std::vector<size_t> indices = shuffledIndices(testImages.dimensions[0]);
     Tensor testBatch = retrieveBatchFromData(testImages, indices);
@@ -137,51 +71,39 @@ void testModel( Tensor& testImages ) {
         reconstructedImages.data[i] *= 255;
     }
 
+    std::filesystem::create_directory( dirForImages );
+
     for (size_t i = 0; i < 10; i++) {
-        convertTensorToPng(testBatch, i, "../test/orig_" + std::to_string(i) + ".png");
-        convertTensorToPng(reconstructedImages, i, "../test/reconstructed_" + std::to_string(i) + ".png");
+        convertTensorToPng(testBatch, i, dirForImages + "/orig_" + std::to_string(i) + ".png");
+        convertTensorToPng(reconstructedImages, i, dirForImages + "/reconstructed_" + std::to_string(i) + ".png");
     }
 }
 
+// void generateImages() {
+//     Reparameterize reparam;
 
-void generateImages() {
-    Reparameterize reparam;
+//     Model decoder {
+//         LinearLayer(latent, neurons),
+//         ReLU(),
+//         LinearLayer(neurons, pixelsInImage),
+//         Sigmoid()
+//     };
 
-    Model decoder {
-        LinearLayer(latent, neurons),
-        ReLU(),
-        LinearLayer(neurons, pixelsInImage),
-        Sigmoid()
-    };
+//     decoder.load("../decoder.model");
 
-    decoder.load("../decoder.model");
+//     const size_t amountOfImages = 10;
+//     Tensor normDistWithLogVariance = Tensor::zeros({amountOfImages, latent * 2});
+//     reparam.forward(normDistWithLogVariance);
+//     Tensor& output = decoder.forward(reparam.outputTensor);
+//     for (size_t i = 0; i < output.length; i++) {
+//         output.data[i] *= 255;
+//     }
+//     Reshape reshaper({amountOfImages, 3, 64, 64});
+//     reshaper.forward(output);
+//     Tensor& images = reshaper.outputTensor;
 
-    const size_t amountOfImages = 10;
-    Tensor normDistWithLogVariance = Tensor::zeros({amountOfImages, latent * 2});
-    reparam.forward(normDistWithLogVariance);
-    Tensor& output = decoder.forward(reparam.outputTensor);
-    for (size_t i = 0; i < output.length; i++) {
-        output.data[i] *= 255;
-    }
-    Reshape reshaper({amountOfImages, 3, 64, 64});
-    reshaper.forward(output);
-    Tensor& images = reshaper.outputTensor;
-
-    for (size_t i = 0; i < images.dimensions[0]; i++) {
-        convertTensorToPng(images, i, std::string("../newCats/cat_" + std::to_string(i) + ".png"));
-    }
+//     for (size_t i = 0; i < images.dimensions[0]; i++) {
+//         convertTensorToPng(images, i, std::string("../newCats/cat_" + std::to_string(i) + ".png"));
+//     }
     
-}
-
-
-void testModel(Model& model) {
-
-    Model decoder {
-        LinearLayer(latent, neurons),
-        ReLU(),
-        LinearLayer(neurons, pixelsInImage),
-        Sigmoid()
-    };
-
-    decoder.load("../decoder.model");
-}
+// }
