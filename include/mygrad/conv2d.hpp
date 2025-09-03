6c83212d93f5b1012f8b8e7396c180ba34f8c100
matraass11 <fedor.kurmanov@gmail.com>
2025-09-03 13:03:52 +0200

    cleaned things up in im2col and the new forward, completely replaced the old forward with the im2col version

--- include/mygrad/conv2d.hpp
+++ include/mygrad/conv2d.hpp
@@ -13,28 +13,19 @@ struct Conv2d : Layer {
 
     void print();
 
-    void forward( Tensor& inputTensor ) override {
-        // forward2(inputTensor);
-        forward1(inputTensor);
-    }
-
-    void forward1( Tensor& inputTensor );
-    void forward2( Tensor& inputTensor );
-
+    void forward( Tensor& inputTensor ) override;
     void backward() override;
+
     std::vector<Tensor*> parameterTensors() override { return { &kernels, &biases }; }
     std::vector<Tensor*> nonParameterTensors() override { return { &outputTensor }; }
 
-// private:
-    Tensor matrixFormCurrentInput;
-
-    void im2col( const Tensor& inputTensor, Tensor& matrixFormTensor );
-    void movePatchToMatrixForm( size_t picture, int leftUpperRow, int leftUpperCol, Tensor& matrixFormTensor, size_t rowInMatrixForm );
-    constexpr size_t convolvedSize( size_t size ) noexcept { return (size + 2*paddingSize - kernelSize)/stride + 1; } 
-
 private:
+    Tensor matrixFormInput;
 
-    dtype convolve(size_t pictureIndex, size_t filterIndex, int inputRow, int inputCol) const;
+    void im2col( const Tensor& inputTensor );
+    void movePatchToMatrixForm( size_t picture, int leftUpperRow, int leftUpperCol, Tensor& matrixFormTensor, size_t rowInMatrixForm );
+    
+    constexpr size_t convolvedSize( size_t size ) noexcept { return (size + 2*paddingSize - kernelSize)/stride + 1; } 
     void convolveBackward(size_t pictureIndex, size_t filterIndex, int inputRow, int inputCol, dtype gradPassedDown);
 
     inline void manageDimensions( const Tensor& inputTensor ) override;
