43dd31d67e8c316d7cc3777c41ee292fcd9bc768
matraass11 <fedor.kurmanov@gmail.com>
2025-09-08 14:10:26 +0200

    separated the cli code into its own header, added "generate" to cli options, cleaned up some old unneded code

--- examples/cats/src/modelRunner.hpp
+++ examples/cats/src/modelRunner.hpp
@@ -4,5 +4,5 @@
 #include "processData.hpp"
 
 void trainModel( mygrad::Model& encoder, mygrad::Model& decoder, Dataset& dataset );
-void reconstructImages( mygrad::Model& encoder, mygrad::Model& decoder, mygrad::Tensor& testImages, const std::string& dirForImages);
-void generateImages( mygrad::Model& encoder, mygrad::Model& decoder, size_t latent, const std::string& dirForImages );
+void reconstructImages( mygrad::Model& encoder, mygrad::Model& decoder, mygrad::Tensor& testImages, const std::string& dirForImages );
+void generateImages( mygrad::Model& encoder, mygrad::Model& decoder, size_t latent, const std::string& dirForImages, size_t amountOfImages );
